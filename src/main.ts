7845130eed6a0408a45b4b1290b13b5930e22d61
cvetlicnifractional <86823960+cvetlicnifractional@users.noreply.github.com>
2023-08-07 21:04:54 +0200

    update cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ const cookieSession = require('cookie-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({credentials: true, origin: 'http://localhost:3001'});
+  app.enableCors({credentials: true, origin: true});
 
   app.use(cookieSession({
     keys: ['asdf']
